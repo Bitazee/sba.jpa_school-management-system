4b178904065f80b6a9dd3fca23223f46f8d77c5c
Marcus Katalenas <katalenasm2@mailbx.winthrop.edu>
2020-09-04 13:37:53 -0400

    Annotated Students

--- src/main/java/com/github/perscholas/model/Student.java
+++ src/main/java/com/github/perscholas/model/Student.java
@@ -1,5 +1,53 @@
 package com.github.perscholas.model;
 
-// TODO - Annotate and Implement respective interface and define behaviors
-public class Student {
+import com.sun.javafx.beans.IDProperty;
+
+import javax.persistence.*;
+
+
+@Entity
+@Table(name = "Student")
+public class Student implements  StudentInterface{
+
+    @Id
+    @Column(name = "email")
+    private String email;
+
+    @Basic
+    @Column(name = "name")
+    private String name;
+
+    @Basic
+    @Column(name = "password")
+    private String password;
+
+    @Override
+    public String getEmail() {
+        return email;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
