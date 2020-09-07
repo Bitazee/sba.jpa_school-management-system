ae4c4150724d50c5e5124bb446c802094b4916b6
Marcus Katalenas <katalenasm2@mailbx.winthrop.edu>
2020-09-07 18:10:29 -0400

    JDBCconfig done... maybe soon tm

--- src/main/java/com/github/perscholas/JdbcConfigurator.java
+++ src/main/java/com/github/perscholas/JdbcConfigurator.java
@@ -2,13 +2,15 @@ package com.github.perscholas;
 
 import com.github.perscholas.utils.DirectoryReference;
 import com.github.perscholas.utils.FileReader;
+import com.mysql.cj.jdbc.Driver;
 
 import java.io.File;
+import java.sql.DriverManager;
 
 public class JdbcConfigurator {
     static {
         try {
-            // TODO - Attempt to register JDBC Driver
+            DriverManager.registerDriver(Driver.class.newInstance());
         } catch (Exception e) {
             throw new Error(e);
         }
@@ -30,8 +32,7 @@ public class JdbcConfigurator {
         File creationStatementFile = DirectoryReference.RESOURCE_DIRECTORY.getFileFromDirectory(fileName);
         FileReader fileReader = new FileReader(creationStatementFile.getAbsolutePath());
         String[] statements = fileReader.toString().split(";");
-        for (int i = 0; i < statements.length; i++) {
-            String statement = statements[i];
+        for (String statement : statements) {
             dbc.executeStatement(statement);
         }
     }
